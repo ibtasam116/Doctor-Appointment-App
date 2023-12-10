eb149164156bbaecbabf157a0927435b2f466130
ibtasam116 <ibtasam116@gmail.com>
2023-12-10 19:32:35 +0500

    nIce

--- app/dashboard/adminHome/(components)/topCards/TopCards.js
+++ app/dashboard/adminHome/(components)/topCards/TopCards.js
@@ -1,34 +1,7 @@
 import React from "react";
 import trendImg from "../../../assets/svg/bx_trending-up.svg";
-import Card from "../card/Card";
 import Image from "next/image";
 
-const cardData = [
-  {
-    title: "Patients",
-    percent: "10.0%",
-    trendImg: trendImg,
-    perCss: "text-_green bg-_bgGreen",
-    total: "856",
-    desi: "Active",
-  },
-  {
-    title: "Appointments",
-    percent: "22.0%",
-    trendImg: trendImg,
-    perCss: "text-_green bg-_bgGreen",
-    total: "3,342",
-    desi: "Active",
-  },
-  {
-    title: "Doctors",
-    percent: "12.0%",
-    trendImg: trendImg,
-    perCss: "text-_green bg-_bgGreen",
-    total: "77",
-    desi: "Active",
-  },
-];
 
 export default function TopCards() {
   return (
@@ -56,20 +29,52 @@ export default function TopCards() {
           Active
         </p>
       </div>
-      {/* {cardData.map((item, i) => {
-        return (
-          <Card
-            // key={i}
-            cardCss={item.cardCss}
-            title={item.title}
-            percent={item.percent}
-            trendImg={item.trendImg}
-            perCss={item.perCss}
-            total={item.total}
-            desi={item.desi}
-          />
-        );
-      })} */}
+      <div
+        className={`lg:max-w-[450px] xl:max-w-[300px] max-h-[139px] bg-white border-[2px] border-_borderColor rounded-[6px] p-5 flex flex-col justify-center  flex-shrink-0`}
+      >
+        <div className="flex">
+          <h4 className="text-_cardColor text-[16px] font-[700] leading-normal tracking-[0.16px] flex-shrink-0">
+            Appointments
+          </h4>
+          <span
+            className={`ml-3 pl-0.5 pr-1 flex items-center gap-[3px] bg-opacity-[0.1] rounded-[50px]`}
+          >
+            <Image src={trendImg} alt="Trending Image" />
+            <span className="text-[14px] font-[400] leading-normal tracking-[0.14px]">
+              22.0%
+            </span>
+          </span>
+        </div>
+        <h1 className="text-[28px] font-[700] my-1 leading-normal tracking-[0.28px]">
+          3,342
+        </h1>
+        <p className="text-_card text-[14px] font-[400] leading-normal tracking-[0.14px]">
+          Active
+        </p>
+      </div>
+      <div
+        className={`lg:max-w-[450px] xl:max-w-[300px] max-h-[139px] bg-white border-[2px] border-_borderColor rounded-[6px] p-5 flex flex-col justify-center  flex-shrink-0`}
+      >
+        <div className="flex">
+          <h4 className="text-_cardColor text-[16px] font-[700] leading-normal tracking-[0.16px] flex-shrink-0">
+            Doctors
+          </h4>
+          <span
+            className={`ml-3 pl-0.5 pr-1 flex items-center gap-[3px] bg-opacity-[0.1] rounded-[50px]`}
+          >
+            <Image src={trendImg} alt="Trending Image" />
+            <span className="text-[14px] font-[400] leading-normal tracking-[0.14px]">
+              12.0%
+            </span>
+          </span>
+        </div>
+        <h1 className="text-[28px] font-[700] my-1 leading-normal tracking-[0.28px]">
+          77
+        </h1>
+        <p className="text-_card text-[14px] font-[400] leading-normal tracking-[0.14px]">
+          Active
+        </p>
+      </div>
     </div>
   );
 }
--- app/dashboard/doctor/page.js
+++ app/dashboard/doctor/page.js
@@ -1,26 +1,58 @@
-// "use client";
-// import React, { useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
+
 import Header from "../(components)/Header/Header";
 import SideBar from "../(components)/sideBar/SideBar";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
-import dbConnect from "@/config/db";
-import { doctorModel } from "@/model/doctors";
+// import dbConnect from "@/config/db";
+// import { doctorModel } from "@/model/doctors";
 
-dbConnect();
+// dbConnect();
 
-const fetchData = async () => {
-  try {
-    const data = await doctorModel.find();
-    console.log("data", data);
-    return data;
-  } catch (error) {
-    console.log("error", error);
-  }
-};
-const page = async () => {
-  // const [loading, setLoading] = useState(false);
-  const data = await fetchData();
+// const fetchData = async () => {
+//   try {
+//     const data = await doctorModel.find();
+//     console.log("data", data);
+//     return data;
+//   } catch (error) {
+//     console.log("error", error);
+//   }
+// };
+const page = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpenedit, setIsModalOpenedit] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [student, setstudent] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [isAddingStudent, setIsAddingStudent] = useState(false);
+  const [isUpdatingStudent, setIsUpdatingStudent] = useState(false);
+  const [isDeletingStudent, setIsDeletingStudent] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const closeModaledit = () => {
+    setIsModalOpenedit(false);
+  };
+
+  const fetchUsers = async () => {
+    const response = await fetch("http://localhost:3000/api/doctor");
+    const result = await response.json();
+    console.log("result", result);
+    setData(result.users);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <>
       <Header />
@@ -31,7 +63,7 @@ const page = async () => {
           <h2 className="text-xl font-semibold mb-4 text-center text-white"></h2>
           <button
             className="text-white bg-[#122E99] rounded-lg py-1 px-5 mb-3"
-            // onClick={openModal}
+            onClick={openModal}
           >
             Add +
           </button>
@@ -50,7 +82,10 @@ const page = async () => {
             {data.map((item, i) => {
               return (
                 <>
-                  <tr key={i} className="bg-gray-100 mt-3 text-[#292D32] text-center border-b border-[#122E99]">
+                  <tr
+                    key={i}
+                    className="bg-gray-100 mt-3 text-[#292D32] text-center border-b border-[#122E99]"
+                  >
                     <td className="px-4 py-2">{i + 1}</td>
                     <td className="px-4 py-2">{item.name}</td>
                     <td className="px-4 py-2">{item.specialization}</td>
@@ -76,6 +111,235 @@ const page = async () => {
           </tbody>
         </table>
       </div>
+
+      {isModalOpenedit && (
+        <div className="fixed text-center inset-0 flex items-center justify-center modal-overlay">
+          <div className="fixed inset-0 bg-black opacity-50"></div>
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white p-5 rounded shadow-md w-[90vh]">
+              <div className="flex justify-end">
+                <button
+                  onClick={closeModaledit}
+                  className="text-gray-600 hover:text-gray-800 focus:outline-none"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <h1 className="text-2xl font-semibold mb-6">Update Student</h1>
+              <form
+                // onSubmit={handleUpdate}
+                className="mx-auto"
+              >
+                <div className="mb-6">
+                  <label
+                    htmlFor="title"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                    className="mt-1 p-2 w-full lg:w-3/4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Enter title"
+                    // onChange={(e) => setname(e.target.value)}
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="location"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Student Id
+                  </label>
+                  <input
+                    type="number"
+                    id="studentid"
+                    name="studentid"
+                    // value={studentid}
+                    className="mt-1 p-2 w-full lg:w-3/4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Enter Student Id"
+                    // onChange={(e) => setstudentid(e.target.value)}
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="option"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Select an Option
+                  </label>
+                  <select
+                    id="option"
+                    name="option"
+                    // onChange={(e) => setSelectedCourse(e.target.value)}
+                    className="mt-1 p-2 w-full lg:w-3/4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                  >
+                    <option value="Web and Mobile developnment">
+                      Web and Mobile developnment
+                    </option>
+                    <option value="Graphics Gesigning">
+                      Graphics Gesigning
+                    </option>
+                    <option value="CCNP">CCNP</option>
+                    {/* Add more options as needed */}
+                  </select>
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="description"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Phone No
+                  </label>
+                  <input
+                    type="phone"
+                    id="phone"
+                    name="phone"
+                    // value={phone}
+                    className="mt-1 p-2 w-full lg:w-3/4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Enter Phone No"
+                    onChange={(e) => setphone(e.target.value)}
+                  />
+                  <label
+                    htmlFor="description"
+                    className="block text-sm mt-5 font-medium text-gray-600"
+                  >
+                    Address
+                  </label>
+                  <input
+                    type="text"
+                    id="address"
+                    name="address"
+                    // value={address}
+                    className="mt-1 p-2 w-full lg:w-3/4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Enter Address "
+                    onChange={(e) => setaddress(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-center">
+                  <button
+                    type="submit"
+                    className="bg-orange-500 text-white py-3 w-[75%] px-6 rounded-lg hover:bg-orange-600 focus:outline-none focus:bg-orange-600"
+                  >
+                    {/* {isUpdatingStudent ? "Updating Student..." : "Update Student"} */}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isModalOpen && (
+        <div className="fixed text-center inset-0 flex items-center justify-center modal-overlay">
+          <div className="fixed inset-0 bg-black opacity-50"></div>
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white p-5 rounded shadow-md w-[90vh]">
+              <div className="flex justify-end">
+                <button
+                  onClick={closeModal}
+                  className="text-gray-600 hover:text-gray-800 focus:outline-none"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <h1 className="text-2xl font-semibold mb-6">Add Doctor</h1>
+              <form
+                // onSubmit={handelSubmit}
+                className="mx-auto"
+              >
+                <div className="mb-6">
+                  <label
+                    htmlFor="title"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="mt-1 p-2 w-full lg:w-3/4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Enter Doctor Name"
+                    onChange={(e) => setname(e.target.value)}
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="location"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Specialization
+                  </label>
+                  <input
+                    type="text"
+                    id="specialization"
+                    name="specialization"
+                    className="mt-1 p-2 w-full lg:w-3/4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Enter Doctor Specialization"
+                    onChange={(e) => setstudentid(e.target.value)}
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="description"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Email
+                  </label>
+
+                  <input
+                    type="phone"
+                    id="phone"
+                    name="phone"
+                    className="mt-1 p-2 w-full lg:w-3/4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Enter Doctor Email"
+                    onChange={(e) => setphone(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-center">
+                  <button
+                    type="submit"
+                    className="bg-[#122E99] text-white py-3 w-[75%] px-6 rounded-lg hover:bg-[#122E99] focus:outline-none focus:bg-[#122E99]"
+                  >
+                    Add Doctor
+                    {/* {isAddingStudent ? "Adding Student..." : "Add Student"} */}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
