3296b6ae2dd0c1b7b32aaeda59e289a6b792e5aa
ibtasam116 <ibtasam116@gmail.com>
2023-12-10 18:17:45 +0500

    Now

--- app/dashboard/adminHome/(components)/topCards/TopCards.js
+++ app/dashboard/adminHome/(components)/topCards/TopCards.js
@@ -1,6 +1,7 @@
 import React from "react";
 import trendImg from "../../../assets/svg/bx_trending-up.svg";
 import Card from "../card/Card";
+import Image from "next/image";
 
 const cardData = [
   {
@@ -32,7 +33,30 @@ const cardData = [
 export default function TopCards() {
   return (
     <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6 xl:gap-0 md:ml-10">
-      {cardData.map((item, i) => {
+      <div
+        className={`lg:max-w-[450px] xl:max-w-[300px] max-h-[139px] bg-white border-[2px] border-_borderColor rounded-[6px] p-5 flex flex-col justify-center  flex-shrink-0`}
+      >
+        <div className="flex">
+          <h4 className="text-_cardColor text-[16px] font-[700] leading-normal tracking-[0.16px] flex-shrink-0">
+            Patients
+          </h4>
+          <span
+            className={`ml-3 pl-0.5 pr-1 flex items-center gap-[3px] bg-opacity-[0.1] rounded-[50px]`}
+          >
+            <Image src={trendImg} alt="Trending Image" />
+            <span className="text-[14px] font-[400] leading-normal tracking-[0.14px]">
+              10.0%
+            </span>
+          </span>
+        </div>
+        <h1 className="text-[28px] font-[700] my-1 leading-normal tracking-[0.28px]">
+          856
+        </h1>
+        <p className="text-_card text-[14px] font-[400] leading-normal tracking-[0.14px]">
+          Active
+        </p>
+      </div>
+      {/* {cardData.map((item, i) => {
         return (
           <Card
             // key={i}
@@ -45,7 +69,7 @@ export default function TopCards() {
             desi={item.desi}
           />
         );
-      })}
+      })} */}
     </div>
   );
 }
